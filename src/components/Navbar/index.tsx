ae2241d8efe9ecf8146e24fc053186b3ce53b16f
Gahh-gon <gabriel000.g.g.p@gmail.com>
2024-09-25 00:03:09 -0300

    Navbar com estilo

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom"
+import LogoImg from '../../assets/imgs/Logo/logoLampada.svg'
 export default function Navbar(){
 return(
-   <nav>
-    <h1 className="text-4xl font-bold text-blue-600">Navbar</h1>
-    <ul>
-      <li><Link to={"/"}>About</Link></li>
+   <nav className=" flex flex-col md:flex-row items-center justify-around fixed top-0 left-0 right-0 bg-black text-white bg-opacity-20 z-10 p-2">
+         <Link to={"/"}>
+        <div className="flex items-center ">
+        <img src={LogoImg} alt="" className="w-32"/>
+        <h1 className="text-4xl font-bold">Oneshot</h1>
+        </div>
+        </Link>
+    <ul className="flex space-x-6 text-lg md:text-2xl">
       <li><Link to={"/characters"}>Characters</Link></li>
       <li><Link to={"/gameinfo"}>Game Info</Link></li>
       <li><Link to={"/gamingcontent"}>Gaming Conten</Link></li>

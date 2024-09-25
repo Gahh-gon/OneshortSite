5e011a0ca8fb61de78f64ed6cd3705a9282909a0
Gahh-gon <gabriel000.g.g.p@gmail.com>
2024-09-24 23:46:39 -0300

    tailwind funcional

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // Isso cobre todos os arquivos da pasta "src"
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'bannerFundo-image': "url('./src/assets/imgs/banner/fundo.svg')",
+      },
+    },
   },
   plugins: [],
 }
